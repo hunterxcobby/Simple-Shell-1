afdb5c7c745a4439592cf7630e84f4711da92f25
hunterxcobby <solomonsefah13@gmail.com>
2023-11-10 09:02:42 +0000

    fixed betty

--- others/str_functwo.c
+++ others/str_functwo.c
@@ -1,24 +1,25 @@
 #include "shell.h"
 
 /**
- * _strdup - Duplicate a string.
+ * strdup_function - Duplicate a string.
  * @str: The string to duplicate.
  *
  * Return: A pointer to the duplicated string, or NULL if allocation fails.
  */
 char *strdup_function(const char *str)
 {
-    char *dup; /* The duplicate string*/
-    size_t len;
-    if (str == NULL)
-        return (NULL);
+	char *dup; /* The duplicate string*/
+	size_t len;
 
-    len = strlen_function(str);
-    dup = (char *)malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
 
-    if (dup == NULL)
-        return NULL;
+	len = strlen_function(str);
+	dup = (char *)malloc(len + 1);
 
-    strcpy(dup, str);
-    return (dup);
+	if (dup == NULL)
+		return (NULL);
+
+	strcpy(dup, str);
+	return (dup);
 }

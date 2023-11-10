b90d8c33d6a6ba4a21acd67825421ed7602bc6a4
hunterxcobby <solomonsefah13@gmail.com>
2023-11-10 08:25:51 +0000

    a function to change working directory

---
+++ others/chdir.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * cd_function - Change current directory.
+ * @path: The path .
+ */
+void cd_function(char *path)
+{
+	char *new_dir;
+
+	if (!path || *path == '\0')
+		path = getenv("HOME");
+	else if (strcmp(path, "-") == 0)
+		path = getenv("OLDPWD");
+
+	if (chdir(path) != 0)
+	{
+		perror("chdir");
+		return;
+	}
+
+	new_dir = getcwd(NULL, 0);
+
+	if (new_dir != NULL)
+	{
+		setenv("PWD", new_dir, 1);
+		free(new_dir);
+	}
+	else
+	{
+		perror("change directory failed");
+	}
+}
